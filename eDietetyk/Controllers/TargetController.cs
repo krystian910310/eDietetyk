b5ff9a86906775890fa03ee0303a7bb3059c99db
krystian910310 <krystian910310@gmail.com>
2017-01-13 22:27:59 +0100

    Poprawki

---
+++ eDietetyk/Controllers/TargetController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eDietetyk.Controllers
+{
+    [Authorize]
+    public class TargetController : Controller
+    {
+        // GET: Chart
+        public ActionResult GetPanel()
+        {
+            return PartialView("Panel");
+        }
+    }
+}
